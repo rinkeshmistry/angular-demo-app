9720d025624fffa53e1166f4256ae94bb20b3da5
Rinkesh Mistry <rinkesh_mistry@hotmail.com>
2022-07-24 11:45:11 +0530

    Resolve Enviourment Import Related Issue

--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map, Observable, Subject } from 'rxjs';
 import { EmployeeModel } from './employee.model';
-import { environment } from 'src/environments/environment';
+import { environment } from '../environments/environment';
 import { KeyValue } from '@angular/common';
 
 
