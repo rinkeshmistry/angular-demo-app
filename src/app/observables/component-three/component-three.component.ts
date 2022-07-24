cb5c7234445ca8d154c1ac2d128150cac663f3b9
rinkeshmistry <rinkesh_mistry@hotmail.com>
2022-07-24 11:38:33 +0530

    1 file modified

--- src/app/observables/component-three/component-three.component.ts
+++ src/app/observables/component-three/component-three.component.ts
@@ -1,19 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { EmployeeService } from 'src/app/employee.service';
 
 @Component({
   selector: 'app-component-three',
   templateUrl: './component-three.component.html',
-  styleUrls: ['./component-three.component.scss']
+  styleUrls: ['./component-three.component.scss'],
 })
 export class ComponentThreeComponent implements OnInit {
+  newNumber: string = '';
 
-  newNumber:string='';
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-    
-  }
-
+  ngOnInit(): void {}
 }
